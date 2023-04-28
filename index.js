b657f6d3bdc00c60457a174af44280376f40e301
Kylych <asankulov.kylych@gmail.com>
2023-04-28 13:56:43 +0600

    disabled mongodb

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require('express');
-const authRoutes = require('./auth/routes');
-const postRouter = require('./posts');
+// const authRoutes = require('./auth/routes');
+// const postRouter = require('./posts');
 
-const { isAuthenticated } = require('./auth/auth');
+// const { isAuthenticated } = require('./auth/auth');
 
 
 const app = express();
@@ -23,8 +23,8 @@ app.use((request, response, next) => {
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use(authRoutes);
-app.use(isAuthenticated, postRouter);
+// app.use(authRoutes);
+// app.use(isAuthenticated, postRouter);
 
 app.get('/', (request, response) => {
   response.render(
@@ -33,7 +33,7 @@ app.get('/', (request, response) => {
   );
 });
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log('Http server started on port 3000');
 });
 
